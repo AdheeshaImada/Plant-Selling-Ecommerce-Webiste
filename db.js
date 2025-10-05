d4c57c888f35aaf14f4b9517e1f5edda5760c397
AdheeshaImada <adheeshaimadakulasekara@gmail.com>
2025-10-05 06:16:52 +0530

    railway db conection updated

--- db.js
+++ db.js
@@ -1,15 +1,19 @@
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '24254',
-  database: 'plant_shop'
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
-db.connect((err) => {
-  if (err) throw err;
-  console.log('🌿 Connected to MySQL');
+connection.connect((err) => {
+  if (err) {
+    console.error('❌ Database connection failed:', err.stack);
+    return;
+  }
+  console.log('✅ Connected to Railway MySQL database.');
 });
 
-module.exports = db;
+module.exports = connection;
