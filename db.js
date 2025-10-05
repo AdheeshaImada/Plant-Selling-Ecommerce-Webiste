a9852535f2ba03794e32f36c40e231bde1d82b84
AdheeshaImada <adheeshaimadakulasekara@gmail.com>
2025-10-05 08:34:01 +0530

    url2

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: "hopper.proxy.rlwy.net",
-  user: "root",
-  password: "YwABBpYUckRrdxokHEAhdaUdfMxvveUp",
-  database: "railway",
-  port: 10213
+  host: process.env.MYSQLHOST,     // Should be internal: 'mysql.railway.internal'
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQLPORT       // Should be internal: 3306
 });
 
 connection.connect((err) => {
